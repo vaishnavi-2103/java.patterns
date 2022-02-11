a047dae28fe07cd092dc326bbcfcd40fb1200533
vaishnavi-2103 <68457139+vaishnavi-2103@users.noreply.github.com>
2022-02-11 12:26:58 +0530

    Add files via upload

---
+++ pattern6.java
@@ -0,0 +1,12 @@
+public class pattern6 {
+    public static void main(String[]args){
+        int k=1;
+        for(int i=1;i<=5;i++){
+            for(int j=0;j<5;j++){
+                System.out.print(k +" ");
+                k=k+1;
+            }
+            System.out.println(" ");
+        }
+    }
+}
---
+++ pattern8.java
@@ -0,0 +1,12 @@
+public class pattern8 {
+    public static void main(String[]args){
+        int k=2;
+        for(int i=1;i<=5;i++){
+            for(int j=0;j<5;j++){
+                System.out.print(k +" ");
+                k=k+2;
+            }
+            System.out.println(" ");
+        }
+    }
+}
